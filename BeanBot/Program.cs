fc07365e6f495a22585c699e506def5c62aca0bd
Aldrin Dominique Natividad <aldmnatividad@gmail.com>
2020-11-30 23:45:30 -0500

    Change Program.cs

--- BeanBot/Program.cs
+++ BeanBot/Program.cs
@@ -2,8 +2,10 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,16 +13,65 @@ namespace BeanBot
 {
     public class Program
     {
+        public static string logDirectory;
+
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            logDirectory = Path.Combine("BeanBotFiles", "Logs");
+
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Verbose()
+                .WriteTo.Console()
+                .WriteTo.Async(a => a.File(Path.Combine(logDirectory, "BeanBotLogs.txt"), rollingInterval: RollingInterval.Day))
+                .CreateLogger();
+            Log.Information("Logger Configuration complete");
+
+            try
+            {
+                Log.Information("Starting up");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Application start-up failed");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSerilog()
+                .ConfigureHostConfiguration(configHost =>
+                {
+                    configHost.SetBasePath(Directory.GetCurrentDirectory());
+                    configHost.AddJsonFile("Config/hostSettings.json", optional: true);
+                    configHost.AddEnvironmentVariables(prefix: "BOT_");
+                    configHost.AddCommandLine(args);
+                })
+                .ConfigureAppConfiguration((hostContext, configApp) =>
+                {
+                    configApp.AddJsonFile("Config/appSettings.json", optional: true);
+                    configApp.AddJsonFile($"Config/appSettings.{hostContext.HostingEnvironment.EnvironmentName}.json", optional: true);
+                    configApp.AddEnvironmentVariables(prefix: "BOT_");
+                    configApp.AddCommandLine(args);
+                })
+                .ConfigureLogging((hostContext, configLogging) =>
+                {
+                    configLogging.AddConfiguration(hostContext.Configuration.GetSection("Logging"));
+                    configLogging.AddConsole();
+                    configLogging.AddDebug();
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
-                });
+                    webBuilder.UseStartup<Startup>().ConfigureLogging(logging =>
+                    {
+                        logging.AddConsole();
+                        logging.AddDebug();
+                    });
+                })
+                .UseConsoleLifetime();
     }
 }
